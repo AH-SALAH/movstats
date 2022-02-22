2115e4eda3e861e6f5d740d5b7a1d590c8772874
AH-SALAH <ah.9alah@gmail.com>
2022-02-22 22:17:47 +0400

    🦄 refactor: add link to logo

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -8,7 +8,11 @@ const Nav = () => {
     return (
         <div className="w-full flex place-content-start place-items-center px-14 py-5">
             <div className="text-2xl font-bold text-zinc-100 mr-10 flex place-content-start place-items-center gap-2">
-                <Image src={'/android-chrome-192x192.png'} width={50} height={50} alt={'logo'} layout={'fixed'} /> {AppConfig.title}
+                <Link href="/">
+                    <a className="gap-2 text-zinc-100 border-none flex place-items-center">
+                        <Image src={'/android-chrome-192x192.png'} width={50} height={50} alt={'logo'} layout={'fixed'} /> {AppConfig.title}
+                    </a>
+                </Link>
             </div>
             <ul className="flex flex-wrap text-lg place-content-center place-items-end">
                 <li className="mr-6">
@@ -21,7 +25,7 @@ const Nav = () => {
                 <li className="mr-6">
                     <Link href="/stats">
                         <a className="text-zinc-100 hover:text-indigo-600 border-none flex place-items-center gap-1">
-                            <ChartBarIcon width={25} height={25}/> <span>Stats</span>
+                            <ChartBarIcon width={25} height={25} /> <span>Stats</span>
                         </a>
                     </Link>
                 </li>
