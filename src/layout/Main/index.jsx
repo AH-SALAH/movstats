e0d349eccbf85f8a5beff6a9c80feed45af5214b
AH-SALAH <ah.9alah@gmail.com>
2022-02-22 20:59:47 +0400

    🌈 style: add goey placeholder

--- src/layout/Main/index.jsx
+++ src/layout/Main/index.jsx
@@ -1,6 +1,6 @@
 const Main = ({ children }) => {
     return (
-        <main className="mx-auto max-w-screen-lg py-20">
+        <main className="mx-auto max-w-screen-lg py-20" style={{background: `url('/assets/images/goey.svg') no-repeat scroll center center/contain`}}>
             {children}
         </main>
     )
