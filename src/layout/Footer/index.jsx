78810e79eda8bd9cf3a73a5d141eed8478a7759f
AH-SALAH <ah.9alah@gmail.com>
2022-02-23 18:00:20 +0400

    🌈 style: change bg-color

--- src/layout/Footer/index.jsx
+++ src/layout/Footer/index.jsx
@@ -2,7 +2,7 @@ import { AppConfig } from '@/utils/AppConfig';
 import Moviedb from "@/public/assets/images/moviedb.svg";
 
 const Footer = () => (
-    <footer className="py-8 bg-indigo-700 text-white flex place-content-center place-items-center text-sm text-center border-t border-gray-300">
+    <footer className="py-8 bg-[#483d8b] text-white flex place-content-center place-items-center text-sm text-center border-t border-gray-300">
         © Copyright {new Date().getFullYear()} {AppConfig.title}. {' '}
         {/* <Image src={moviedb} width={100} height={50} alt={'moviedb'} /> */}
         <Moviedb width={100} height={50} />
