216857ff339193ab659a772135ec555d3888e1ed
AH-SALAH <ah.9alah@gmail.com>
2022-02-23 13:05:36 +0400

    🦄 refactor: add default height

--- src/components/Pagination/index.jsx
+++ src/components/Pagination/index.jsx
@@ -1,7 +1,7 @@
 import Pagination from '@etchteam/next-pagination';
 import '@etchteam/next-pagination/dist/index.css';
 
-const PaginationC = ({total_pages=1000, size=undefined, theme=null}) => {
+const PaginationC = ({total_pages=1000, size=20, theme=null}) => {
   return (
     <Pagination total={total_pages} size={size} theme={theme} />
   )
