a57310a80e803d3a9c52245ec20c6f237070b0b5
AH-SALAH <ah.9alah@gmail.com>
2022-02-24 09:15:02 +0400

    🎈 perf: enhance nav

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -3,32 +3,48 @@ import Image from "next/image";
 import { AppConfig } from '@/utils/AppConfig';
 import HomeIcon from '@heroicons/react/outline/HomeIcon';
 import ChartBarIcon from '@heroicons/react/outline/ChartBarIcon';
+import navStyle from './menu.module.scss';
+import { useRouter } from "next/router";
+
+
+let navLinks = [
+    {
+        title: 'Home',
+        href: '/',
+        icon: <HomeIcon width={25} height={25} />
+    },
+    {
+        title: 'Stats',
+        href: '/stats',
+        icon: <ChartBarIcon width={25} height={25} />
+    },
+];
 
 const Nav = () => {
+    let router = useRouter();
+    console.log("router: ", router);
     return (
         <div className="w-full flex place-content-center md:place-content-start place-items-center px-14 py-5 flex-wrap">
-            <div className="text-2xl font-bold text-zinc-100 md:mr-10 flex place-content-start place-items-center gap-2">
+            <div className="text-2xl font-bold text-zinc-100 text-center w-full md:w-auto md:mr-10 flex place-content-center md:place-content-start place-items-center gap-2">
                 <Link href="/">
                     <a className="gap-2 text-zinc-100 border-none flex place-items-center">
                         <Image src={'/android-chrome-192x192.png'} width={50} height={50} alt={'logo'} layout={'fixed'} /> {AppConfig.title}
                     </a>
                 </Link>
             </div>
-            <ul className="flex flex-wrap text-lg place-content-center place-items-end pt-6 md:pt-0 text-center">
-                <li className="mr-6">
-                    <Link href="/">
-                        <a className="text-zinc-100 hover:text-indigo-600 border-none flex place-items-center gap-1">
-                            <HomeIcon width={25} height={25} />
-                        </a>
-                    </Link>
-                </li>
-                <li className="mr-6">
-                    <Link href="/stats">
-                        <a className="text-zinc-100 hover:text-indigo-600 border-none flex place-items-center gap-1">
-                            <ChartBarIcon width={25} height={25} /> <span>Stats</span>
-                        </a>
-                    </Link>
-                </li>
+            <ul className={`${navStyle.menu} flex flex-wrap text-lg place-content-center place-items-end pt-6 md:pt-0 text-center`}>
+                {
+                    navLinks?.map(l => (
+                            <li key={l.title} className="mr-6">
+                                <Link href={`${l.href}`}>
+                                    <a className={`${router.asPath === l.href && navStyle.activeLink || ''} text-zinc-100 hover:text-indigo-600 border-none flex place-items-center gap-1`}>
+                                        {l.icon} <span>{l.title}</span>
+                                    </a>
+                                </Link>
+                            </li>
+                        )
+                    )
+                }
             </ul>
         </div>
     )
